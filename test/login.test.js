4e7d113910a91a21469b895d0ed10aee00d27005
Pedro Filipe  Tavares Baptista <109918486+pedrofi96@users.noreply.github.com>
2025-10-15 23:10:39 -0300

    Iniciando projeto, com teste para rota Post do ending point /login

---
+++ test/login.test.js
@@ -0,0 +1,24 @@
+import request from 'supertest'; //Para que se possa fazer requisições a API
+import { expect } from 'chai'; //Para validar o que foi obtido de resposta e o valor que se esperava
+
+
+  describe('Login', () => { //mocha para estruturar os testes e executar eles.
+    describe('POST /login', ()=>{
+      it('Deve retornar 200 com token em string quando usar credenciais válidas', async ()=>{
+        const response = await request('http://localhost:3000')
+          .post('/login')
+          .set('Content-Type', 'application/json')
+          .send({
+            'username':'julio.lima',
+            'senha':'123456'
+          })
+
+        expect(response.status).to.equal(200);
+        expect(response.body.token).to.be.a('string');
+
+      })
+    })
+
+
+
+  })
